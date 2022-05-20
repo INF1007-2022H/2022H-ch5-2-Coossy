342b5cfb7aed3bbf9b13723083bc045ca8150f6e
Coossy <charles-emile.cossette@polymtl.ca>
2022-05-20 12:18:55 -0400

    exercice1

--- exercice.py
+++ exercice.py
@@ -7,8 +7,19 @@ def get_bill(name, data):
 	INDEX_NAME = 0
 	INDEX_QUANTITY = 1
 	INDEX_PRICE = 2
+	
+	taux_taxes = (15/100)
+	sous_total = 0
 
-	return ""
+	for elem in data:
+			sous_total += elem[INDEX_QUANTITY] * elem[INDEX_PRICE]
+
+	taxes = (sous_total * (taux_taxes))
+	montant_total = sous_total + taxes
+	
+	facture = [name, ("SOUS-TOTAL", "%.2f" % sous_total), ("TAXES", "%.2f" % taxes), ("TOTAL", "%.2f" % montant_total)]
+
+	return facture
 
 
 def format_number(number, num_decimal_digits):
